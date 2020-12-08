4617fbd9ec42915b56163001efe2cee6aa86fbe9
mjmccoach <mmccoach88@gmail.com>
2020-12-08 16:20:37 +0000

    Added an extra component

---
+++ src/components/SongSelector.js
@@ -0,0 +1,7 @@
+const SongSelector = () => {
+    return(
+        <h3>I'm the Song Selector</h3>
+    )
+}
+
+export default SongSelector;
--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -1,10 +1,21 @@
+import {useState} from 'react';
 import SongList from '../components/SongList';
+import SongSelector from '../components/SongSelector';
 
 const SongContainer = () => {
+
+    const [song, setSong] = useState(1)
+
+    const getSong = () => {
+        fetch(`https://itunes.apple.com/gb/rss/topsongs/limit=20/json`)
+        .then(res => res.json())
+        .then(data => setSong(data))
+    }
     return (
         <>
         <h1>I'm the Song Container</h1>
         <SongList />
+        <SongSelector />
         </>
     )
 }
