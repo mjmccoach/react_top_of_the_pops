ca38ffecd883aecb2b44e9d14ada75ff1b8b09d7
mjmccoach <mmccoach88@gmail.com>
2020-12-08 16:08:48 +0000

    Basic components skeletoned, registed and rendered

---
+++ src/components/SongList.js
@@ -0,0 +1,7 @@
+const SongList = () => {
+    return (
+        <h2>I'm the SongList</h2>
+    )
+}
+
+export default SongList;
---
+++ src/containers/SongContainer.js
@@ -0,0 +1,12 @@
+import SongList from '../components/SongList';
+
+const SongContainer = () => {
+    return (
+        <>
+        <h1>I'm the Song Container</h1>
+        <SongList />
+        </>
+    )
+}
+
+export default SongContainer;
